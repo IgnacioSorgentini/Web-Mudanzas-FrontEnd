3c17c3f0767dc033466d42c5ef129c593120cb69
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-10-19 15:08:11 -0300

    Arrancando estructura de la pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="app-izquierda">
+        <div className="app-imagen">
+          
+        </div>
+      </div>
+      <div className="app-derecha">
+          <section class="spikes">
+            <div className="app-titulo">
+              <h1>MeMudo!</h1>
+            </div>
+            <div className="app-descripcion">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum alias consectetur ad, quis delectus neque culpa tempore totam dolor, iure facilis beatae odit natus, esse dolorum ex molestiae tenetur! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ad eveniet maiores dolores placeat, dolor aliquid laboriosam totam eligendi blanditiis molestias amet magnam reiciendis molestiae ullam corporis labore. Iste, blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non labore possimus corrupti quibusdam assumenda aspernatur veritatis fuga quaerat saepe, iure magni repellat numquam sunt mollitia, voluptatibus sapiente esse reprehenderit.
+            </div>
+          </section>
+          <div className="app-botones">
+            <button type="button" class="btn btn-lg btn-warning">Iniciar sesion</button>
+            <button type="button" class="btn btn-lg btn-warning">Registrarse</button>
+          </div>
+      </div>
     </div>
   );
 }
