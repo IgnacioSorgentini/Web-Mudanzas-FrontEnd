0de177521477767b50fdcbfc82d14cb660550392
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-10-20 17:31:12 -0300

    Agregando ventana Inicio Sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css';
+import {useState} from 'react';
+import InicioSesion from './pages/InicioSesion/InicioSesion';
 
 function App() {
+
+  const [buttonPopupIniciarSesion, setButtonPopupIniciarSesion] = useState(false);
+
+
   return (
     <div className="App">
+      <div className="contenedor-general">
       <div className="app-izquierda">
         <div className="app-imagen">
           
@@ -14,14 +21,16 @@ function App() {
               <h1>MeMudo!</h1>
             </div>
             <div className="app-descripcion">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum alias consectetur ad, quis delectus neque culpa tempore totam dolor, iure facilis beatae odit natus, esse dolorum ex molestiae tenetur! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ad eveniet maiores dolores placeat, dolor aliquid laboriosam totam eligendi blanditiis molestias amet magnam reiciendis molestiae ullam corporis labore. Iste, blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non labore possimus corrupti quibusdam assumenda aspernatur veritatis fuga quaerat saepe, iure magni repellat numquam sunt mollitia, voluptatibus sapiente esse reprehenderit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             </div>
           </section>
           <div className="app-botones">
-            <button type="button" class="btn btn-lg btn-warning">Iniciar sesion</button>
+            <button type="button" class="btn btn-lg btn-warning" onClick={() => setButtonPopupIniciarSesion(true)}>Iniciar sesion</button>
             <button type="button" class="btn btn-lg btn-warning">Registrarse</button>
           </div>
       </div>
+      </div>
+      <InicioSesion trigger={buttonPopupIniciarSesion} setTrigger={setButtonPopupIniciarSesion}></InicioSesion>
     </div>
   );
 }
--- src/pages/InicioSesion/InicioSesion.jsx
+++ src/pages/InicioSesion/InicioSesion.jsx
@@ -1,12 +1,75 @@
 import React from "react";
+import {useState, useEffect} from 'react';
 import './InicioSesion.css';
 
-function InicioSesion(){
-    return(
-        <div className="inicioSesion">
+function InicioSesion(props){
+    
+    const [validar, setValidar] = useState(-1);
+    const [user, setUser] = useState('');   
+    const [password,setPassword] = useState('')
+    const [error,setError] = useState(1)
 
+
+    const userValido='pedroseveri@gmail.com'
+    const pwValida= '123'
+    
+    const handleUserChange = (e) => {
+    	setUser(e.target.value);
+  	};
+  
+	const handlePasswordChange = (e) => {
+    	setPassword(e.target.value);
+  	};
+
+    function comprobarUsuario () {
+        if (userValido === user && pwValida === password ){
+            setValidar(0)
+        }
+        else{
+            setError(0)
+        }
+    }
+
+    const comprobarUsuarioConEnter = (e) => {
+        if (e.key == 'Enter') {
+            comprobarUsuario()
+          }
+    }
+    
+    if(validar == -1){
+    return(props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <div className="caja-titulo">
+                    <h2 className="title"> Inicio de sesión </h2>
+                </div>
+                <div className="caja-contenido">
+                    <text className="texto"> Ingrese su mail: </text>
+                    <br/>
+                    <input className="input" type ="text" placeholder="Mail" onChange={handleUserChange}/>
+                    <br/>
+                    <br/>
+                    <text className="texto">Ingrese su contraseña:</text>
+                    <br/>
+                    <input className="input" type ="password" placeholder="Contraseña" onChange={handlePasswordChange} onKeyDown={comprobarUsuarioConEnter}/>
+                    <a  className="vinculo" href="#">¿Olvidaste tu contraseña?</a> 
+                    <br/>
+                    {error == 0 && <div className="contenedorError"><text className="error">Los datos ingresados no son correctos</text></div>}
+                    <br/>
+                    <div className="caja-botones">
+                        <button className="boton btn btn-warning btn-lg" onClick={comprobarUsuario}>
+                            INGRESAR
+                        </button>
+                        <button className="boton btn btn-warning btn-lg" onClick={() => props.setTrigger(false)}>
+                            CANCELAR
+                        </button>
+                    </div> 
+                </div>
+
+            </div>
         </div>
-    )
+    ) : "";
+    }
 }
 
-export default InicioSesion();
+export default InicioSesion;
