305a351ef3ee2b6ca197b83ac20eea14c5371964
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-11-09 12:57:40 -0300

    Avanzando con la ventana Editar Empresa

--- src/pages/HomeOferente/HomeOferente.jsx
+++ src/pages/HomeOferente/HomeOferente.jsx
@@ -23,6 +23,29 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Divider from '@mui/material/Divider';
+
+
+//Estas tres funciones son para la Transfer List que está en la ventana Editar.
+function not(a, b) {
+    return a.filter((value) => b.indexOf(value) === -1);
+  }
+  function intersection(a, b) {
+    return a.filter((value) => b.indexOf(value) !== -1);
+  }
+  function union(a, b) {
+    return [...a, ...not(b, a)];
+  }
+
+
+
 
 export default function Empresa(){
     
@@ -37,14 +60,109 @@ export default function Empresa(){
 
 
     const [openEditar, setOpenEditar] = React.useState(false);
-
     const handleClickOpenEditar = () => {
     setOpenEditar(true);
     };
-    
     const handleCloseEditar = () => {
     setOpenEditar(false);
     };
+
+
+
+
+    //Acá arranca un bloque de código que es todo relacionado a la Transfer List.
+    const [checked, setChecked] = React.useState([]);
+    const [left, setLeft] = React.useState(['Embalaje', 'Descenso de pertenencias', 'Guardado de pertenencias', 'Mudanza compartida']); //Acá van TODAS las caractertisticas que consideremos adecuadas a tomar en cuenta.
+    const [right, setRight] = React.useState([]);
+    const leftChecked = intersection(checked, left);
+    const rightChecked = intersection(checked, right);
+    const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    setChecked(newChecked);
+    };
+    const numberOfChecked = (items) => intersection(checked, items).length;
+    const handleToggleAll = (items) => () => {
+        if (numberOfChecked(items) === items.length) {
+          setChecked(not(checked, items));
+        } else {
+          setChecked(union(checked, items));
+        }
+      };
+    const handleAllRight = () => {
+        setRight(right.concat(left));
+        setLeft([]);
+    };
+    const handleCheckedRight = () => {
+        setRight(right.concat(leftChecked));
+        setLeft(not(left, leftChecked));
+        setChecked(not(checked, leftChecked));
+    };
+    const handleCheckedLeft = () => {
+        setLeft(left.concat(rightChecked));
+        setRight(not(right, rightChecked));
+        setChecked(not(checked, rightChecked));
+    };
+    const handleAllLeft = () => {
+        setLeft(left.concat(right));
+        setRight([]);
+    };
+    const customList = (items, title) => (
+        <Card>
+          <CardHeader
+            sx={{ px: 2, py: 1 }}
+            subheader={`${title}`}
+          />
+          <Divider />
+          <List
+            sx={{
+              width: 200,
+              height: 230,
+              bgcolor: 'background.paper',
+              overflow: 'auto',
+            }}
+            dense
+            component="div"
+            role="list"
+          >
+            {items.map((value) => {
+              const labelId = `transfer-list-all-item-${value}-label`;
+    
+              return (
+                <ListItem
+                  key={value}
+                  role="listitem"
+                  button
+                  onClick={handleToggle(value)}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      checked={checked.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{
+                        'aria-labelledby': labelId,
+                      }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={`${value}`} />
+                </ListItem>
+              );
+            })}
+            <ListItem />
+          </List>
+        </Card>
+      );
+    //Acá termina el bloque de código relacionado a la Transfer List
+
+
+
+
 
 
     return(
@@ -101,25 +219,103 @@ export default function Empresa(){
                         </Box>
                     </div>
                     <div style={{display:"flex",minWidth:"100%", minHeight:"20%", justifyContent:"end", alignItems:"center", boxSizing:"border-box", paddingRight:"10px"}}><Button variant="contained" style={{backgroundColor:"#FD841F"}} size="large" endIcon={<EditIcon />} onClick={handleClickOpenEditar}>Editar</Button></div>
-                    <Dialog open={openEditar} onClose={handleCloseEditar}>
+                    <Dialog open={openEditar} onClose={handleCloseEditar} maxWidth="sm">
                         <DialogTitle>Editar informacion de la empresa</DialogTitle>
                         <DialogContent>
                             <DialogContentText>
-                                Esta es la ventana para editar. Estaría bueno poner una Transfer List para elegir las caracteristicas que figuren en el perfil de la empresa.
+                                Modificar caracteristicas de la empresa.
                             </DialogContentText>
-                            <TextField
+                            <div>
+                                <Grid container spacing={2} justifyContent="center" alignItems="center">
+                                    <Grid item>{customList(left, 'Disponibles')}</Grid>
+                                    <Grid item>
+                                        <Grid container direction="column" alignItems="center">
+                                            <Button
+                                                sx={{ my: 0.5 }}
+                                                variant="outlined"
+                                                size="small"
+                                                onClick={handleCheckedRight}
+                                                disabled={leftChecked.length === 0}
+                                                aria-label="move selected right"
+                                                >
+                                                    &gt;
+                                            </Button>
+                                            <Button
+                                                sx={{ my: 0.5 }}
+                                                variant="outlined"
+                                                size="small"
+                                                onClick={handleCheckedLeft}
+                                                disabled={rightChecked.length === 0}
+                                                aria-label="move selected left"
+                                            >
+                                                &lt;
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item>{customList(right, 'Seleccionadas')}</Grid>
+                                </Grid>
+                            </div>
+                            <div style={{marginTop:"50px"}}>
+                            <DialogContentText>
+                                Modificar descripcion de la empresa.
+                                <TextField
                                 autoFocus
-                                margin="dense"
+                                margin=""
                                 id="name"
-                                label="Email Address"
+                                label="Descripcion"
                                 type="email"
                                 fullWidth
                                 variant="standard"
                             />
+                            </DialogContentText>
+                            </div>
+                            <div style={{marginTop:"50px"}}>
+                            <DialogContentText>
+                                Modificar numero de telefono de la empresa.
+                                <TextField
+                                autoFocus
+                                margin=""
+                                id="name"
+                                label="Telefono"
+                                type="email"
+                                fullWidth
+                                variant="standard"
+                            />
+                            </DialogContentText>
+                            </div>
+                            <div style={{marginTop:"50px"}}>
+                            <DialogContentText>
+                                Modificar direccion de la empresa.
+                                <TextField
+                                autoFocus
+                                margin=""
+                                id="name"
+                                label="Direccion"
+                                type="email"
+                                fullWidth
+                                variant="standard"
+                            />
+                            </DialogContentText>
+                            </div>
+                            <div style={{marginTop:"50px"}}>
+                            <DialogContentText>
+                                Modificar direccion de correo electronico de la empresa.
+                                <TextField
+                                autoFocus
+                                margin=""
+                                id="name"
+                                label="Mail"
+                                type="email"
+                                fullWidth
+                                variant="standard"
+                            />
+                            </DialogContentText>
+                            </div>
+                            
                         </DialogContent>
                         <DialogActions>
-                            <Button onClick={handleCloseEditar}>Cancel</Button>
-                            <Button onClick={handleCloseEditar}>Subscribe</Button>
+                            <Button onClick={handleCloseEditar}>Confirmar cambios</Button>
+                            <Button onClick={handleCloseEditar}>Cancelar</Button>
                         </DialogActions>
                     </Dialog>
                 </div>
