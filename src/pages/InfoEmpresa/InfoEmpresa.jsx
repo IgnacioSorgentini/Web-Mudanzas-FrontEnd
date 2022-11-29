18b2067b131ab579db3265c16fb1474e60af6f1a
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-11-29 14:56:00 -0300

    Corrigiendo ultimo detalle de los commits anteriores

--- src/pages/InfoEmpresa/InfoEmpresa.jsx
+++ src/pages/InfoEmpresa/InfoEmpresa.jsx
@@ -280,7 +280,7 @@ export default function Empresa(){
                             </div>
                         </div>
                         
-                        <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
+                        <div style={{display:"flex",flexDirection:"row",justifyContent:"space-between", alignItems:"center"}}>
                             <Rating value={location.state.puntuacion} readOnly/>
                             <Button style={{color:"black"}} onClick={handleClickOpenCompartir}><ShareIcon style={{fontSize:20}}/></Button>
                             <Dialog
