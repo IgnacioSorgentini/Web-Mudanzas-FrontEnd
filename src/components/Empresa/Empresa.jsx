b84119829e0c280d82bc8eba47c1b638a1845451
Ignacio Sorgentini <ignaciosorgentini7@gmail.com>
2022-11-29 13:05:46 -0300

    Mejorando componente Empresa

--- src/components/Empresa/Empresa.jsx
+++ src/components/Empresa/Empresa.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import './Empresa.css';
 import Rating from '@mui/material/Rating';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { Chip } from "@mui/material";
+import BusinessIcon from '@mui/icons-material/Business';
 
 function Empresa({nombre, puntuacion, ubicacion, precioXKm}){
     return(
         <div className="contenedor-empresa">
             <div className="contenedor-imagen">
-
+                <Chip label="Simulador!" color="error" size="small" style={{margin:"5px"}} />
             </div>
             <div className="contenedor-nombre">
-                <p>{nombre}</p>
+            <Chip icon={<BusinessIcon/>} label={`${nombre}`}/>
+            </div>
+            <div className="contenedor-ubicacion">
+                <Chip icon={<LocationOnIcon/>} label={`${ubicacion}`}/>
             </div>
             <div className="contenedor-puntuacion">
                 <Rating name="read-only" value={puntuacion}  readOnly/>
-            </div>
-            <div className="contenedor-ubicacion">
-                <p><LocationOnIcon></LocationOnIcon>{ubicacion}</p>
             </div>
         </div>
     )
